9edfafaf92d988702bc8f518edf71f278c091075
ProgrammerShri <shrikantdewangan3@gmail.com>
2022-03-14 18:00:29 +0530

    Favicon change and url

--- src/components/Medias/Seo.js
+++ src/components/Medias/Seo.js
@@ -1,37 +1,47 @@
-import React from 'react';
-import { Helmet } from 'react-helmet'
-import banner from './Banner/banner.jpg'
+import React from "react";
+import { Helmet } from "react-helmet";
+import banner from "./Banner/banner.jpg";
 
 const Seo = () => {
-  return (<>
+  return (
+    <>
+      <Helmet>
+        {/* <!-- Primary Meta Tags --> */}
+        <title>Shrikant Dewangan</title>
+        <meta name="title" content="Shrikant Dewangan" />
+        <meta
+          name="description"
+          content="A Web UI Front-End Developer.
+                    I help companies and organizations build modern, resilient, and inclusive Web user interfaces with a strong focus on responsive design, performance, and accessibility."
+        />
 
-            <Helmet>
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://programmershri.web.app/" />
+        <meta property="og:title" content="Shrikant Dewangan" />
+        <meta
+          property="og:description"
+          content="A Web UI Front-End Developer.
+                I help companies and organizations build modern, resilient, and inclusive Web user interfaces with a strong focus on responsive design, performance, and accessibility."
+        />
+        <meta property="og:image" content={banner} />
 
-                {/* <!-- Primary Meta Tags --> */}
-                <title>Cedrick Lupembe</title>
-                <meta name="title" content="Cedrick Lupembe" />
-                <meta name="description" content="A Web UI Front-End Developer.
-                    I help companies and organizations build modern, resilient, and inclusive Web user interfaces with a strong focus on responsive design, performance, and accessibility." />
-
-                {/* <!-- Open Graph / Facebook --> */}
-                <meta property="og:type" content="website" />
-                <meta property="og:url" content="https://cedricklupembe.netlify.com/" />
-                <meta property="og:title" content="Cedrick Lupembe" />
-                <meta property="og:description" content="A Web UI Front-End Developer.
-                I help companies and organizations build modern, resilient, and inclusive Web user interfaces with a strong focus on responsive design, performance, and accessibility." />
-                <meta property="og:image" content={banner} />
-
-                {/* <!-- Twitter --> */}
-                <meta property="twitter:card" content="summary_large_image" />
-                <meta property="twitter:url" content="https://cedricklupembe.netlify.com/" />
-                <meta property="twitter:title" content="Cedrick Lupembe" />
-                <meta property="twitter:description" content="A Web UI Front-End Developer.
-                I help companies and organizations build modern, resilient, and inclusive Web user interfaces with a strong focus on responsive design, performance, and accessibility." />
-                <meta property="twitter:image" content={banner} />
-
-            </Helmet>
-
-        </>
-)};
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://twitter.com/programmershri"
+        />
+        <meta property="twitter:title" content="Shrikant Dewangan" />
+        <meta
+          property="twitter:description"
+          content="A Web UI Front-End Developer.
+                I help companies and organizations build modern, resilient, and inclusive Web user interfaces with a strong focus on responsive design, performance, and accessibility."
+        />
+        <meta property="twitter:image" content={banner} />
+      </Helmet>
+    </>
+  );
+};
 
 export default Seo;
